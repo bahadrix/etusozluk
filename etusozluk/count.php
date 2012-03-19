74efc258d457141cbb88c5a2b2bd3734949752b0
Bahadir <bahadrix@gmail.com>
2012-03-19 23:32:52 +0200

    Entry sayma modülüne gun özelliği eklendi

--- etusozluk/count.php
+++ etusozluk/count.php
@@ -10,6 +10,16 @@ switch ($say) {
     case 'dun':
         $date_condition = "entries.Tarih BETWEEN ADDDATE(CURDATE(), INTERVAL - 1 DAY) AND CURDATE()";
         break;
+    case 'gun':
+        // parametreler girilmiş mi?
+        if (!isset($_GET['gun']) || !isset($_GET['ay']) || !isset($_GET['yil'])) die('tarih parametresi eksik');
+        // dogru mu girilmis peki?
+        if (!checkdate($_GET['ay'], $_GET['gun'], $_GET['yil'])) die('gecersiz tarih');
+        // tarihi olustur
+        $tarih = "{$_GET['yil']}-{$_GET['ay']}-{$_GET['gun']}";
+        // yardir
+        $date_condition = "entries.Tarih BETWEEN '$tarih'  AND ADDDATE('$tarih', INTERVAL  1 DAY)";
+        break;
     case 'bugun':
         $date_condition = "entries.Tarih BETWEEN CURDATE() AND ADDDATE(CURDATE(), INTERVAL  1 DAY)";
         break;
