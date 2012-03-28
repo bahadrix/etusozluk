58d70d75183c6768e0850f6f6f5b90811986c91a
Bahadir Katipoglu <bahadrix@gmail.com>
2012-03-28 19:35:54 +0300

    Başlık beslemesi tamam

--- etusozluk/data/baslik.php
+++ etusozluk/data/baslik.php
@@ -5,64 +5,21 @@ $gun = $_GET['g'];
 $per_page = 3; // Per page records
 
 
+$next_start = ($page - 1) * $per_page; 
+
 $date_condition = $gun == 'gun' ? 
 				getCondByDate($gun, 'titles.tarih', $_GET['gun'], $_GET['ay'], $_GET['yil'] ) : 
 				getCondByDate($gun, 'titles.tarih');
 
 
 $DBO = getPDO();
-$titles = $DBO->query("SELECT titles.T_ID as id, titles.Baslik as baslik FROM titles LIMIT 50");
+$titles = $DBO->query("SELECT titles.T_ID as id, titles.Baslik as baslik FROM titles WHERE $date_condition LIMIT $next_start,$per_page");
 
 
-echo json_encode($titles->fetchAll(PDO::FETCH_ASSOC));
+echo json_encode(
+		array('items' =>
+			$titles->fetchAll(PDO::FETCH_ASSOC)
+				)
+		);
 
-
-
-
-/*
-
-
-$JSON["items"] = array();
-
-if ($gun == "bugun") {
-$arr = array();
-if ($page==1) {
-  $arr = array("id" => "1234", "baslik" => "ilk sayfa başlıkları", "count" => "10");
-  array_push($JSON["items"],$arr);
-  array_push($JSON["items"],$arr);
-  array_push($JSON["items"],$arr);
-  echo json_encode($JSON);
-}
-if ($page==2) {
-  $arr = array("id" => "1324", "baslik" => "ikinci sayfa başlıkları", "count" => "2");
-  array_push($JSON["items"],$arr);
-  array_push($JSON["items"],$arr);
-  array_push($JSON["items"],$arr);
-  echo json_encode($JSON);
-}
-}
-else if ($gun == "dun") {
-$arr = array();
-if ($page==1) {
-  $arr = array("id" => "1234", "baslik" => "dün ilk sayfa başlıkları", "count" => "10");
-  array_push($JSON["items"],$arr);
-  array_push($JSON["items"],$arr);
-  array_push($JSON["items"],$arr);
-  echo json_encode($JSON);
-}
-if ($page==2) {
-  $arr = array("id" => "1324", "baslik" => "dün ikinci sayfa başlıkları", "count" => "2");
-  array_push($JSON["items"],$arr);
-  array_push($JSON["items"],$arr);
-  array_push($JSON["items"],$arr);
-  echo json_encode($JSON);
-}
-if ($page==3) {
-  $arr = array("id" => "1324", "baslik" => "dün üçüncü sayfa başlıkları", "count" => "1");
-  array_push($JSON["items"],$arr);
-  array_push($JSON["items"],$arr);
-  echo json_encode($JSON);
-}
-}
-*/
 ?>
