06127d1a92cc688d468d44135e7dd39ad7b3cc9f
Umutcan Guney <umutcang@gmail.com>
2012-03-31 15:26:46 +0300

    yazarmini yenilendi.

--- etusozluk/yazarmini.php
+++ etusozluk/yazarmini.php
@@ -1,28 +1,24 @@
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-</head><body>
-<?php
+﻿<?php
+include_once('common.php');
+include_once('funct.php');
 $msg="";
-if (isset($_POST['yid'])) {
+if (!empty($_POST['yid']) && is_int($_POST['yid'])) {
   $yid = $_POST['yid'];
-  if ($yid == "1234") {
-	$msg .= "<a href=yazar.php?nick=veritas>veritas</a> <br />";
-	$msg .= "Mesaj Gönder <br />"; //giriş yapıldıysa
-	$msg .= "Son Giriş: Tarih <br />";
-	$msg .= "Son Girdi <br />";
-  }
-  else {
-	$msg .= "<a href=yazar.php?nick=tahrik+eden+cisim>tahrik eden cisim</a> <br />";
-	$msg .= "Mesaj Gönder <br />"; //giriş yapıldıysa
-	$msg .= "Son Giriş: Tarih <br />";
-	$msg .= "Son Girdi <br />";
-  }
-  }
-else {
-  $msg .= "Hatalı Sorgu";
-  }
+  $link = getPDO();
+  $st = $link -> prepare('SELECT U_ID,Nick,Uyelik_Tarihi,Sehir,Son_Online FROM members where U_ID=:uid');
+  $st -> bindValue(":uid",$yid);
+  $st -> execute();
+  if ($st->rowCount() == 1) {
+	$sonuc = $st -> fetch(PDO::FETCH_ASSOC);
+    $msg .= '<a href="yazar.php?y='.yazarBoslukSil($sonuc['Nick']).'" style="color:#0081c6; font-weight:bold;">'.$sonuc['Nick'].'</a><hr />';
+	if ($MEMBER_LOGGED && $_SESSION['member']->U_ID != $sonuc['U_ID'])
+	  $msg .= '<a href="mesaj.php?y='.yazarBoslukSil($sonuc['Nick']).'">Mesaj Gönder</a><br />';
+	$msg .= '<a href="baslik.php?y='.yazarBoslukSil($sonuc['Nick']).'">Son Yazdıkları</a><br />'; //baslik.php'ye burayı ekleriz sonra.
+	$msg .= 'Son Giriş: '.substr($sonuc['Son_Online'],0,10).'<br />';
+	$msg .= 'Üyelik Tarihi: '.substr($sonuc['Uyelik_Tarihi'],0,10).'<br />';
+  } else
+      $msg .= "Geçersiz Üye"; //buraya düşmemesi lazım.
+} else
+    $msg .= "Hatalı Sorgu";
 echo $msg;
-echo '</body></html>';
 ?>
-</body></html>
