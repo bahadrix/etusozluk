dca5fff0e527e9b801ecb524a51d5dc3b6495624
Umutcan Guney <umutcang@gmail.com>
2012-04-05 15:05:55 +0300

    pae.js başlık link gösterme düzeltildi.

--- etusozluk/js/pae.js
+++ etusozluk/js/pae.js
@@ -534,6 +534,11 @@ function generateRows(selected, opt) {
 			return /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(a)
 		};
 		
+		function boslukSil(a) {
+			var baslik = a.replace(/ /g,"+");
+			return baslik;
+		}
+		
 		function ep( s,w,h ) {
 		if (!h) h = 200;
 		if (!w) w = 320;
@@ -626,7 +631,7 @@ function generateRows(selected, opt) {
 							var sayi='';
 							if (item.count>1)
 							sayi='('+item.count+')';
-							$("#basliklar").append('<li class="b">-&nbsp;<a href='+item.id+'>'+item.baslik+'</a> '+sayi+'</li>');
+							$("#basliklar").append('<li class="b">-&nbsp;<a href=goster.php?t='+boslukSil(item.baslik)+'>'+item.baslik+'</a> '+sayi+'</li>');
 						}
 					});
 					$("#basliklar").append('</ul>');
@@ -670,7 +675,7 @@ function generateRows(selected, opt) {
 							var sayi='';
 							if (item.count>1)
 							sayi='('+item.count+')';
-							$("#basliklar").append('<li class="b">-&nbsp;<a href='+item.id+'>'+item.baslik+'</a> '+sayi+'</li>');
+							$("#basliklar").append('<li class="b">-&nbsp;<a href=goster.php?t='+boslukSil(item.baslik)+'>'+item.baslik+'</a> '+sayi+'</li>');
 						}
 					});
 					$("#basliklar").append('</ul>');
